64e8ce9b0ceab97b7326a379e7e5fab0df74823f
samjain109 <samyakjain2732000#gmail.com>
2026-09-23 17:08:24 +0530

    Add Flight Management System microservice

---
+++ Flight-Management-System/src/main/java/com/system/api/gateway/FlightManagementSystemApplication.java
@@ -0,0 +1,5 @@
+package com.system.api.gateway;
+
+public class FlightManagementSystemApplication {
+
+}
---
+++ Flight-Management-System/src/main/java/com/system/api/gateway/FlightManagementSystemApplicationDemo.java
@@ -0,0 +1,8 @@
+package com.system.api.gateway;
+
+public class FlightManagementSystemApplicationDemo {
+
+	public static void main(String[] args) {
+	}
+
+}
