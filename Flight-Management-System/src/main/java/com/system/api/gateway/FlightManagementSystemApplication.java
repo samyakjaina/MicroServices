feeb4f58d83100bd2f427027ed00510dffab9d50
samjain109 <samyakjain2732000#gmail.com>
2026-09-23 17:09:32 +0530

    remove Flight Management System microservice

--- Flight-Management-System/src/main/java/com/system/api/gateway/FlightManagementSystemApplication.java
+++
@@ -1,5 +0,0 @@
-package com.system.api.gateway;
-
-public class FlightManagementSystemApplication {
-
-}
--- Flight-Management-System/src/main/java/com/system/api/gateway/FlightManagementSystemApplicationDemo.java
+++
@@ -1,8 +0,0 @@
-package com.system.api.gateway;
-
-public class FlightManagementSystemApplicationDemo {
-
-	public static void main(String[] args) {
-	}
-
-}
